b3e6c956bdc12e770447c67308cfefccbcef568d
aleclamble <71652814+aleclamble@users.noreply.github.com>
2024-11-25 22:41:09 +0200

    fix: remove test code causing Vercel build failure

--- src/lib/assembly.ts
+++ src/lib/assembly.ts
@@ -1,11 +1,7 @@
 import { AssemblyAI } from 'assemblyai';
-import { readFileSync, writeFileSync } from 'fs'
 const client = new AssemblyAI({
     apiKey: process.env.ASSEMBLYAI_API_KEY!,
 });
-
-const FILE_URL =
-    'https://assembly.ai/sports_injuries.mp3';
 
 function msToTime(ms: number): string {
     const seconds = ms / 1000;
@@ -13,7 +9,6 @@ function msToTime(ms: number): string {
     const remainingSeconds = Math.floor(seconds % 60);
     return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
 }
-
 
 export const processMeeting = async (audio_url: string) => {
     const transcript = await client.transcripts.transcribe({
@@ -33,25 +28,4 @@ export const processMeeting = async (audio_url: string) => {
     return {
         transcript, summaries
     }
-
 };
-
-// processMeeting(FILE_URL);
-const fileUrl = '/Users/elliott/Downloads/lob.mp3'
-
-const transcript = await client.transcripts.transcribe({
-    // audio_url: fileUrl,
-    audio: readFileSync(fileUrl),
-    auto_chapters: true,
-    language_code: 'en',
-});
-
-await writeFileSync('transcript.json', JSON.stringify(transcript, null, 2))
-
-// const summaries = transcript.chapters?.map(chapter => ({
-//     start: msToTime(chapter.start),
-//     end: msToTime(chapter.end),
-//     gist: chapter.gist,
-//     headline: chapter.headline,
-//     summary: chapter.summary
-// })) || [];
